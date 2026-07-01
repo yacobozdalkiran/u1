34fd3ecdc2b3d85e8614180c910caacbf1d979b8
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-07-01 11:55:30 +0200

    Fix comments per algo

--- src/ecmc/ecmc.h
+++ src/ecmc/ecmc.h
@@ -14,7 +14,7 @@ struct ECMCParams {
     double theta_sample = 100;
     double theta_refresh = 50;
     int algo = 0;       // 0: classic ecmc 1: topological lifting
-    double eta = 1e-6;  // Only if algo=1
+    double eta = 1e-6;  // Only used if algo=1
 };
 
 /**
@@ -45,65 +45,77 @@ struct Distributions {
 };
 
 /**
- * @brief Optimized function to solve the reject equation.
+ * @brief Optimized function to solve the reject equation. (algo 0, 1)
  */
 void solve_reject_fast(double A, double B, double& gamma, double& reject, int epsilon);
 
 /**
- * @brief Computes the values of the 2 plaquettes attached to a link.
+ * @brief Computes the values of the 2 plaquettes attached to a link. (algo 0, 1)
  */
 void compute_plaquettes(const GaugeField& field, const Geometry& geo, int site, int mu,
                         std::array<double, 2>& list_plaquettes);
 
 /**
- * @brief Computes the values of the reject angles for each plaquette attached to a link.
+ * @brief Computes the values of the reject angles for each plaquette attached to a link. (algo 0, 1)
  */
 void compute_reject_angles_fast(const std::array<double, 2>& list_plaquettes, int epsilon,
                                 const double& beta, std::array<double, 2>& reject_angles,
                                 std::mt19937_64& rng);
 
 /**
- * @brief Uses the tower of probability method to choose an index in a length 3 array.
+ * @brief Uses the tower of probability method to choose an index in a length 3 array. (algo 0, 1)
  */
 size_t selectVariable_norev(const std::array<double, 3>& probas, std::mt19937_64& rng);
 
 /**
- * @brief Lift without backtracking from a link.
+ * @brief Updates a link. (algo 0, 1)
+ */
+void update(GaugeField& field, int site, int mu, double theta, int epsilon);
+
+// =========== Algo 0 : Forward lifts ===========
+
+/**
+ * @brief Lift without backtracking from a link. (algo 0)
  */
 std::pair<std::pair<int, int>, int> lift_improved_fast_norev(const GaugeField& field,
                                                              const Geometry& geo, int site, int mu,
                                                              int j, std::mt19937_64& rng);
 
+
+// =========== Algo 1 : Topology-driven lifts ===========
+
 /**
- * @brief Returns the bistochastic version of a 4x4 matrix.
+ * @brief Returns the bistochastic version of a 4x4 matrix. (algo 1)
  */
 void sinkhorn_knopp(double W[4][4], int iterations = 15);
 
 /**
- * @brief Returns the sum of the absolute value of local topological charge of the 2 plaquettes attached to a link normalized to [0,1].
+ * @brief Returns the sum of the absolute value of local topological charge of the 2 plaquettes attached to a link normalized to [0,1]. (algo 1)
  */
 double get_topological_variation(const GaugeField& field, const Geometry& geo, int site, int mu);
 
 /**
- * @brief Performs a topology-driven lift from a link.
+ * @brief Performs a topology-driven lift from a link. (algo 1)
  */
 std::pair<std::pair<int, int>, int> lift_topological(const GaugeField& field, const Geometry& geo,
                                                      int site, int mu, int j,
                                                      const ECMCParams& params,
                                                      std::mt19937_64& rng);
 
-/**
- * @brief Updates a link.
- */
-void update(GaugeField& field, int site, int mu, double theta, int epsilon);
+
+// =========== Algo 2 : Plaquette ECMC ===========
+
+
+
+// =========== All Algos ===========
 
 /**
- * @brief Selects a random site in the lattice.
+ * @brief Selects a random site in the lattice. (all algos)
  */
 int random_site(const Geometry& geo, std::mt19937_64& rng);
 
 /**
- * @brief Performs the ECMC algorithm until theta_sample is reached.
+ * @brief Performs the ECMC algorithm until theta_sample is reached. (all algos)
  */
 void ecmc_sample(LocalChainState& state, GaugeField& field, double beta, Distributions& d,
                  const Geometry& geo, const ECMCParams& params, std::mt19937_64& rng);
